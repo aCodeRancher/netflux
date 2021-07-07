2b86685bc50c597dc066562c1c88d6636aa50f39
helen <hhma@alumni.cpp.edu>
2021-07-07 17:12:42 -0400

    add a contextload test

--- src/test/java/guru/springframework/netflux/NetfluxApplicationTests.java
+++ src/test/java/guru/springframework/netflux/NetfluxApplicationTests.java
@@ -1,13 +1,23 @@
 package guru.springframework.netflux;
 
+import guru.springframework.netflux.domain.Movie;
+import guru.springframework.netflux.repositories.MovieRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 @SpringBootTest
 class NetfluxApplicationTests {
 
+    @Autowired
+    MovieRepository movieRepository;
+
     @Test
     void contextLoads() {
+        Flux<Movie> movieFlux = movieRepository.findAll();
+        movieFlux.subscribe( movie -> {System.out.println(movie.getTitle());});
+
     }
 
 }
