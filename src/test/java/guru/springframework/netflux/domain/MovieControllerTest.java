c5433f78933584938668f08d4cae08bba3cce72e
helen <hhma@alumni.cpp.edu>
2021-08-05 16:15:55 -0400

    use WebTestClient to test the flux streaming

---
+++ src/test/java/guru/springframework/netflux/domain/MovieControllerTest.java
@@ -0,0 +1,39 @@
+package guru.springframework.netflux.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+class MovieControllerTest {
+
+
+     @Autowired
+     private WebTestClient webTestClient;
+
+
+     @Test
+     void testGetMovieEvent() throws InterruptedException {
+
+         CountDownLatch countDownLatch = new CountDownLatch(2);
+          List<Movie> moviesList = webTestClient.get().uri("/movies").exchange()
+                 .expectBodyList(Movie.class).returnResult().getResponseBody();
+          Movie firstMovie = moviesList.get(0);
+          final  String id = firstMovie.getId();
+
+
+          webTestClient.get().uri("/movies/"+firstMovie.getId()+"/events")
+                   .accept(MediaType.TEXT_EVENT_STREAM)
+                   .exchange().returnResult(MovieEvent.class).getResponseBody().take(2)
+                    .subscribe( movies -> { movies.getMovieId().equals(id); countDownLatch.countDown();} );
+
+          countDownLatch.await();
+          assertThat(countDownLatch.getCount()).isEqualTo(0);
+     }
+}
