5ca208178ef20128c3f324fae82d452aca2baca0
helen <hhma@alumni.cpp.edu>
2021-08-22 16:07:05 -0400

    remove unused import

--- src/test/java/guru/springframework/netflux/domain/MovieControllerTest.java
+++ src/test/java/guru/springframework/netflux/domain/MovieControllerTest.java
@@ -2,6 +2,7 @@ package guru.springframework.netflux.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureWebTestClient
 class MovieControllerTest {
 
 
